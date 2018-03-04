5d832449516d443e15a7c6fc4dacbeb2b539392e
Nada-t <nadatarik2011@hotmail.com>
2018-03-04 02:26:49 +0200

    commit

---
+++ UVA/UVA 10815.cpp
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+#include <cstdio>
+using namespace std;
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+string bin(int n)
+{
+	string s = "";
+	while (n)
+	{
+		s += ((n % 2) + '0');
+		n /= 2;
+	}
+	return s;
+}
+int in(string s)
+{
+	int x = 0;
+	for (int i = 0;i < s.size();i++)
+	{
+		if (s[i] == '1')
+		{
+			x += pow(2, i);
+		}
+	}
+	return x;
+}
+int  main()
+{
+	fast();
+
+	string s;
+	vector <string> v;
+	map<string, int> mp;
+
+	while (getline(cin, s))
+	{
+		string ss = "";
+	
+		for (int i = 0;i < s.size();i++)
+		{
+			if (isalpha(s[i]))
+			{
+				ss +=tolower(s[i]);
+			}
+			else if (ss != "")
+			{
+				if (mp[ss] == 0)
+				{
+					mp[ss]++;
+					v.push_back(ss);
+				}
+				ss = "";
+			}
+		}
+		if (mp[ss] == 0 && ss != "")
+		{
+			mp[ss]++;
+			v.push_back(ss);
+		}
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0;i < v.size();i++)
+	{
+		cout << v[i] << endl;
+	}
+}
