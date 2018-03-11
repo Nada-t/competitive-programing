67daab065be84de799e72400af8f53e229da7cdc
Nada-t <nadatarik2011@hotmail.com>
2018-03-11 21:07:34 +0200

    commit

---
+++ Codeforsces/CF579-D2-A.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+#include <cstdio>
+using namespace std;
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+int  main()
+{
+	fast();
+	int x, sm = 0;
+
+	cin >> x;
+	while (x)
+	{
+		if (x & 1) sm++;
+		x >>= 1;
+	}
+	cout << sm << endl;
+}
